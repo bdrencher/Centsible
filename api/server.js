86aaf432e8701e8d2a219fe19bc974f6b73f1b92
bdrencher <bdrencher@gmail.com>
2020-09-22 19:06:02 -0400

    Configuring the site to use the react build

--- api/server.js
+++ api/server.js
@@ -5,3 +5,7 @@ let app = express();
 app.listen(8080, () => {
     console.log("Listening");
 })
+
+app.get('/', (req, res) => {
+    res.sendFile('/home/ben/Projects/Centsible/client/build/index.html');
+});
