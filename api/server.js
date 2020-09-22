7ad95fc00ea5090be4840fc2834dd1477be49498
bdrencher <bdrencher@gmail.com>
2020-09-22 19:16:25 -0400

    Adding the path library so it can handle pathing for the react application

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.sendFile('/home/ben/Projects/Centsible/client/build/index.html');
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 
