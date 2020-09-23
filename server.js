ed8b872e0f122af482c2a5981bb47aa66f779f30
bdrencher <bdrencher@gmail.com>
2020-09-22 21:04:46 -0400

    allowing the build files to be served

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
