1910a1b69e1b523d4470b8ec434545c44fb718b0
bdrencher <bdrencher@gmail.com>
2020-09-25 22:29:54 -0400

    There is no data returned, so lets try null for the fault test

--- api/databaseHelper/retirementProfilesHelper.js
+++ api/databaseHelper/retirementProfilesHelper.js
@@ -51,7 +51,7 @@ function updateRetirementData(username, profile, callback) {
         values: [profile.currentAge, profile.retirementAge, profile.retirementGoal, profile.currentAssets, username]
     };
     pool.query(query, (err, result) => {
-        if (err || result.rows[0] == null) {
+        if (err || result == null) {
             console.log(err);
             callback(err, false);
         } else {
