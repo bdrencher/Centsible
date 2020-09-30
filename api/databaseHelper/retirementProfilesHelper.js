3b4c9c5299f260a4ad31befea5bd615adf9e0eb1
bdrencher <bdrencher@gmail.com>
2020-09-29 20:38:23 -0400

    the data was being sent as a string, testing now with JSON input

--- api/databaseHelper/retirementProfilesHelper.js
+++ api/databaseHelper/retirementProfilesHelper.js
@@ -20,8 +20,6 @@ const pool = new Pool({
  ********************************************/
 
 function enterRetirementData(username, profile, callback) {
-    console.log(typeof(profile));
-    console.log(JSON.parse(profile).currentAge);
     const query = {
         text: 'INSERT INTO retirement_information VALUES ((SELECT userid FROM users WHERE username = $1), $2, $3, $4, $5)',
         values: [username, profile.currentAge, profile.retirementAge, profile.retirementGoal, profile.currentAssets]
