324a2ad05ee39010a31710e7823210463a3f451a
bdrencher <bdrencher@gmail.com>
2020-10-13 22:00:54 -0400

    modified the calculator to use a slightly different index for each fund

--- client/controller/calculationHelper.js
+++ client/controller/calculationHelper.js
@@ -22,21 +22,33 @@
  *    t = time in years
  *********************************************************/
 module.exports = {
-    
+   calculateInvestment: calculateInvestment, 
 }
 /****************************************
  * @desc calculates compound intrest based
  * on an annual compounding. This does not
  * account for inflation
  * @number principle - The starting investment
- * @number returnOnInvestment - percentage of
- * expected return as a decimal
+ * @number fund - 0 for S&P500, 1 for NASDAQ,
+ * and 2 for Russell 1000
  * @number timeInYears - expected duration
  * of investment in years
  * @boolean accountForInflation - true will
  * decrease ROI by 3%
  ****************************************/
-function calculateInvestment(principle, returnOnInvestment, timeInYears, accountForInflation) {
+function calculateInvestment(principle, fund, timeInYears, accountForInflation) {
+    let returnOnInvestment = 0;
+    switch (fund) {
+        case 0:
+            returnOnInvestment = 0.10;
+            break;
+        case 1:
+            returnOnInvestment = 0.14;
+            break;
+        case 2:
+            returnOnInvestment = 0.12;
+            break;
+    }
     if (accountForInflation) {
         return Math.round(principle * (1 + returnOnInvestment - 0.03)**(timeInYears)); // compounds yearly, accounts for inflation
     } else {
@@ -49,7 +61,7 @@ function calculateInvestment(principle, returnOnInvestment, timeInYears, account
  * for a given fund name, principle, and
  * expected output.
  * @number fundSelection - 0 for SnP500,
- * 1 for Russel1000, and 2 for NASDAQ
+ * 2 for Russel1000, and 1 for NASDAQ
  * @number principle - the initial investment
  * @boolean withInflation - true to test
  * with accounting for inflation
@@ -62,11 +74,11 @@ function testIndexFundResult(fundSelection, principle, withInflation, investment
             interestRate = 0.1;
             fundName = "S&P500";
             break;
-        case 1:
+        case 2:
             interestRate = 0.12;
             fundName = "Russel 1000";
             break;
-        case 2:
+        case 1:
             interestRate = 0.14;
             fundName = "NASDAQ";
             break;
@@ -74,7 +86,7 @@ function testIndexFundResult(fundSelection, principle, withInflation, investment
             break;
     }
 
-    let result = calculateInvestment(principle, interestRate, investmentDuration, withInflation);
+    let result = calculateInvestment(principle, fundSelection, investmentDuration, withInflation);
     if (result == expectedOutput) {
         console.log(fundName + " test passed. Principle: " + principle + ". Years: " + investmentDuration + ". Expected output: " + expectedOutput);
         return true;
@@ -120,19 +132,19 @@ function testNASDAQ() {
     const answer20Inflation  = 2191;
     const answer100Inflation = 10953;
     const answer500Inflation = 54765;
-    testIndexFundResult(2, principle1, true, 45, answer1Inflation);
-    testIndexFundResult(2, principle20, true, 45, answer20Inflation);
-    testIndexFundResult(2, principle100, true, 45, answer100Inflation);
-    testIndexFundResult(2, principle500, true, 45, answer500Inflation);
+    testIndexFundResult(1, principle1, true, 45, answer1Inflation);
+    testIndexFundResult(1, principle20, true, 45, answer20Inflation);
+    testIndexFundResult(1, principle100, true, 45, answer100Inflation);
+    testIndexFundResult(1, principle500, true, 45, answer500Inflation);
     // not accounting for inflation
     const answer1   = 364;
     const answer20  = 7274;
     const answer100 = 36368;
     const answer500 = 181840;
-    testIndexFundResult(2, principle1, false, 45, answer1);
-    testIndexFundResult(2, principle20, false, 45, answer20);
-    testIndexFundResult(2, principle100, false, 45, answer100);
-    testIndexFundResult(2, principle500, false, 45, answer500);
+    testIndexFundResult(1, principle1, false, 45, answer1);
+    testIndexFundResult(1, principle20, false, 45, answer20);
+    testIndexFundResult(1, principle100, false, 45, answer100);
+    testIndexFundResult(1, principle500, false, 45, answer500);
 }
 
 function testRussel1000() {
@@ -145,24 +157,24 @@ function testRussel1000() {
     const answer20Inflation  = 967;
     const answer100Inflation = 4833;
     const answer500Inflation = 24164;
-    testIndexFundResult(1, principle1, true, 45, answer1Inflation);
-    testIndexFundResult(1, principle20, true, 45, answer20Inflation);
-    testIndexFundResult(1, principle100, true, 45, answer100Inflation);
-    testIndexFundResult(1, principle500, true, 45, answer500Inflation);
+    testIndexFundResult(2, principle1, true, 45, answer1Inflation);
+    testIndexFundResult(2, principle20, true, 45, answer20Inflation);
+    testIndexFundResult(2, principle100, true, 45, answer100Inflation);
+    testIndexFundResult(2, principle500, true, 45, answer500Inflation);
     // not accounting for inflation
     const answer1   = 164;
     const answer20  = 3280;
     const answer100 = 16399;
     const answer500 = 81994;
-    testIndexFundResult(1, principle1, false, 45, answer1);
-    testIndexFundResult(1, principle20, false, 45, answer20);
-    testIndexFundResult(1, principle100, false, 45, answer100);
-    testIndexFundResult(1, principle500, false, 45, answer500);
+    testIndexFundResult(2, principle1, false, 45, answer1);
+    testIndexFundResult(2, principle20, false, 45, answer20);
+    testIndexFundResult(2, principle100, false, 45, answer100);
+    testIndexFundResult(2, principle500, false, 45, answer500);
 }
 
 testSnP500();
 testNASDAQ();
 testRussel1000();
 testIndexFundResult(0, 50, false, 32, 1056);
-testIndexFundResult(1, 42, true, 23, 305);
-testIndexFundResult(2, 289347, false, 50, 202610315);
+testIndexFundResult(2, 42, true, 23, 305);
+testIndexFundResult(1, 289347, false, 50, 202610315);
