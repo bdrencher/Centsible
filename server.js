f10beee6750e18cec67a4868d9364b3f7a6b2308
bdrencher <bdrencher@gmail.com>
2020-10-17 20:20:43 -0400

    adding service to static files

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'client', 'src')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
