f438217eddafed4b13b1e6e50a83cc0c1cf2ec29
bdrencher <bdrencher@gmail.com>
2020-11-02 18:44:18 -0500

    added a form and state tracking functions to the goal setter component

--- client/src/components/goalSetter/goalSetter.js
+++ client/src/components/goalSetter/goalSetter.js
@@ -1,15 +1,63 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './goalSetter.module.css';
 
-const goalSetter = () => (
-  <div className={styles.goalSetter}>
-    goalSetter Component
-  </div>
-);
+export class goalSetter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userBirthday: new Date(),
+      timeStamp: new Date(),
+      moneyPerYear: 0,
+      retirementAge: 0,
+      currentAssets: 0
+    }
+  }
 
-goalSetter.propTypes = {};
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
 
-goalSetter.defaultProps = {};
+  handleSubmit = (event) => {
+    this.setState({
+      timeStamp: Date.now(),
+    });
 
-export default goalSetter;
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="goalSetter">
+        <h3>Goals</h3>
+        <p>
+          Please provide the following information about yourself and your
+          financial goals.
+        </p>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Birthdate:
+            <input type="date" name="birthday" value={this.state.userBirthday} onChange={this.handleChange}></input>
+          </label>
+          <label>
+            Desired Retirement Age:
+            <input type="number" name="retirementAge" value={this.state.retirementAge} onChange={this.handleChange}></input>
+          </label>
+          <label>
+            Current assets:
+            <input type="number" name="currentAssets" value={this.state.currentAssets} onChange={this.handleChange}></input>
+          </label>
+          <label>
+            Desired funds per year during retirement:
+            <input type="number" name="moneyPerYear" value={this.state.moneyPerYear} onChange={this.handleChange}></input>
+          </label>
+          <button type="submit" value="Submit">Save goals</button>
+        </form>
+      </div>
+    )
+  }
+}
