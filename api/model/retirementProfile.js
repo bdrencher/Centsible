c59eaa55f1b632b47519d917b4e6de2788ad4510
bdrencher <bdrencher@gmail.com>
2020-11-14 15:38:32 -0500

    making some changes to the retirement profile class

--- api/model/retirementProfile.js
+++ api/model/retirementProfile.js
@@ -1,22 +1,22 @@
 module.exports = 
 /*********************************
  * RetirementProfile: class
- * @param iBirthyear: int
- * @param iRetirementAge: int
- * @param iCurrentAssets: Number
- * @param iRetirementGoal: Number
+ * @Date iBirthdate
+ * @Number iRetirementAge
+ * @Number iCurrentAssets
+ * @Number iRetirementGoal
  * 
  * Used to store a users retirement
  * data
  ********************************/
 class RetirementProfile {
-    birthyear = 0;
-    retirementAge = 0;
-    currentAssets = 0;
-    retirementGoal = 0;
+    birthdate;
+    retirementAge;
+    currentAssets;
+    retirementGoal;
     
-    constructor(iBirthyear, iRetirementAge, iCurrentAssets, iRetirementGoal) {
-        this.birthyear = iBirthyear;
+    constructor(iBirthdate, iRetirementAge, iCurrentAssets, iRetirementGoal) {
+        this.birthdate = iBirthdate;
         this.retirementAge = iRetirementAge;
         this.currentAssets = iCurrentAssets;
         this.retirementGoal = iRetirementGoal;
