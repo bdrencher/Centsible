43897ffc78ca90b2f5760cca8af9e52ace9d39c8
bdrencher <bdrencher@gmail.com>
2020-11-18 19:05:15 -0500

    adding documentation for each function

--- client/src/services/apiCommunicator.js
+++ client/src/services/apiCommunicator.js
@@ -1,8 +1,21 @@
 const axios = require('axios').default;
 
+/*****************************************************
+ * @desc contains all the functions needed to pass data
+ * to and from the database
+ ****************************************************/
 export class ApiCommunicator {
     apiUrlRoot = "https://centsible-finance.herokuapp.com";
 
+    /***********************************************
+     * @desc sends username and password to database
+     * to store the username and store the hash of
+     * the password
+     * 
+     * @string username
+     * @string password
+     * @returns Boolean indicating success or failure
+     **********************************************/
     createUser(username, password) {
         axios.post(apiUrlRoot + '/createUser', {
             username: username,
@@ -16,6 +29,16 @@ export class ApiCommunicator {
         });
     }
 
+    /***********************************************
+     * @desc compares the provided username and
+     * password to the username and
+     * the password (compared to stored passhash) in
+     * the database.
+     * 
+     * @string username
+     * @string password
+     * @returns Boolean indicating success or failure
+     **********************************************/
     validateCredentials(username, password) {
         axios.post(apiUrlRoot + '/validateCredentials', {
             username: username,
@@ -29,6 +52,13 @@ export class ApiCommunicator {
         });
     }
 
+    /***********************************************
+     * @desc deletes the user who activates this
+     * function
+     * 
+     * @string username
+     * @returns Boolean indicating success or failure
+     **********************************************/
     deleteUser(username) {
         axios.delete(apiUrlRoot + '/deleteUser', {
             username: username
@@ -41,6 +71,14 @@ export class ApiCommunicator {
         });
     }
 
+    /***********************************************
+     * @desc sends profile data to the database and
+     * records it under the user's unique id.
+     * 
+     * @string username
+     * @RetirementProfile profile
+     * @returns Boolean indicating success or failure
+     **********************************************/
     createRetirementProfile(username, profile) {
         axios.post(apiUrlRoot + '/createRetirementProfile', {
             username: username,
@@ -54,6 +92,14 @@ export class ApiCommunicator {
         }); 
     }
 
+    /***********************************************
+     * @desc saves new profile data to the unique entry
+     * for the provided user
+     * 
+     * @string username
+     * @RetirementProfile profile
+     * @returns Boolean indicating success or failure
+     **********************************************/
     updateRetirementProfile(username, profile) {
         axios.put(apiUrlRoot + '/updateRetirement', {
             username: username,
@@ -67,6 +113,13 @@ export class ApiCommunicator {
         });
     }
 
+    /***********************************************
+     * @desc extracts retirement profile data from
+     * the database for the provide user
+     * 
+     * @string username
+     * @returns RetirementProfile
+     **********************************************/
     retrieveRetirementProfile(username) {
         axios.post(apiUrlRoot + '/getRetirementProfile', {
             username: username
