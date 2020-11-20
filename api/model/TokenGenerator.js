7e1462352be598ee4f74b1ce4ff3ee54ace1c2db
bdrencher <bdrencher@gmail.com>
2020-11-20 18:14:14 -0500

    adding the token generator class and its implementation. Yes, I know this makes the application a lot less secure to show how the tokens are generated

---
+++ api/model/TokenGenerator.js
@@ -0,0 +1,14 @@
+export class TokenGenerator {
+    token;
+    tokenRules = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-=[]{}?/.,<>|!@#$%^&*()"
+
+    generateToken() {
+        const length = this.tokenRules.length;
+        let newToken = [];
+        for (let i = 0; i < 90; i++) {
+            newToken.push(Math.floor(Math.random() * length));
+        }
+
+        return newToken.join(',');
+    }
+}
