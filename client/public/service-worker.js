89f5e6fa7373a5a061a400473f9ae21cff19529c
bdrencher <bdrencher@gmail.com>
2020-11-26 21:15:35 -0600

    replacing window with self

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -11,16 +11,16 @@
 // opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
+  self.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
+    self.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
+    self.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
 
-window.addEventListener("install", (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(preLoad());
 });
 
@@ -32,7 +32,7 @@ var preLoad = () => {
   });
 };
 
-window.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(checkResponse(event.request).catch(() => {
     return returnFromCache(event.request);
   }));
