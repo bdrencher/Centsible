fef6a5f8b7f5d73aa30efa28632bad4e7085d89f
bdrencher <bdrencher@gmail.com>
2020-12-04 07:03:06 -0700

    adding the other components from recharts to make the barchart work

--- client/src/components/goalProgress/goalProgress.js
+++ client/src/components/goalProgress/goalProgress.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './goalProgress.module.css';
-import { BarChart } from 'recharts';
+import { BarChart, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
 
 export default class GoalProgress extends React.Component {
   constructor(props) {
